adaf9fa44daf90073c07b83cbe31293e04f6f4e4
Andrei Marchenko <ftkvyn@gmail.com>
2018-12-13 15:53:25 +0100

    day 13 progress

--- day-13.py
+++ day-13.py
@@ -1,9 +1,13 @@
+from termcolor import colored
+import colorama
+colorama.init()
+
 class Car:
 
 	def __init__(self, x, y, direction, track):
 		self.x = x
 		self.y = y
-		self.direction = direction
+		self.direction = direction # up down left right
 		self.currentTrack = track
 		self.nextTurn = 'left'
 	
@@ -23,6 +27,7 @@ class BaseTrack:
 		self.x = x
 		self.y = y
 		self.currentCar = currentCar
+		self.direction = '?'
 
 class StraightTrack(BaseTrack):
 
@@ -34,17 +39,112 @@ class StraightTrack(BaseTrack):
 
 class CrossTrack(BaseTrack):
 
-	def __init__(self, x, y, currentCar, left, right, top, bottom):
-		BaseTrack.__init__(self, x, y, currentCar)
+	def __init__(self, x, y, left, right, top, bottom):
+		BaseTrack.__init__(self, x, y, 0)
 		self.left = left
 		self.right = right
 		self.top = top
 		self.bottom = bottom
+		self.direction = '+'
 
-# sums = [[''] * 301 for i in range(301)]
 field = []
+cars = []
+linesNum = 0
+lineLen = 0
+
+def printField():
+	global field, lineLen, linesNum
+	for ln in range(linesNum):
+		for cn in range(lineLen):
+			val = field[ln][cn]
+			if isinstance(val, BaseTrack):
+				if isinstance(val.currentCar, Car):
+					char = '>'
+					if val.currentCar.direction == 'up':
+						char = '^'
+					elif val.currentCar.direction == 'down':
+						char = 'v'
+					elif val.currentCar.direction == 'left':
+						char = '<'
+					print(colored(char, 'green'), end='', )
+				else:
+					print(val.direction, end='')
+			else:
+				print(val, end='')
+		print('')
+	print('')
+
 
 with open('data-13.txt') as f:
 	lines = f.read().splitlines()
-	for _, line in enumerate(lines):
-		vals = line.split(' ')
+	linesNum = len(lines)
+	lineLen = len(lines[0])
+	field = [[''] * lineLen for i in range(linesNum)]
+	for ln, line in enumerate(lines):
+		for cn, char in enumerate(line):
+			item = ' '
+			car = 0
+			if char == '-':
+				item = StraightTrack(cn, ln, 0, '-', field[ln][cn-1], 0)
+				if isinstance(field[ln][cn-1], StraightTrack):
+					field[ln][cn-1].after = item
+				elif isinstance(field[ln][cn-1], CrossTrack):
+					field[ln][cn-1].right = item
+
+			if char == '|':
+				item = StraightTrack(cn, ln, 0, '|', field[ln-1][cn], 0)
+				if isinstance(field[ln-1][cn], StraightTrack):
+					field[ln-1][cn].after = item
+				elif isinstance(field[ln-1][cn], CrossTrack):
+					field[ln-1][cn].bottom = item
+
+			if char == '>' or char == '<':
+				item = StraightTrack(cn, ln, 0, '-', field[ln][cn-1], 0)
+				if isinstance(field[ln][cn-1], StraightTrack):
+					field[ln][cn-1].after = item
+				elif isinstance(field[ln][cn-1], CrossTrack):
+					field[ln][cn-1].right = item
+
+				direction = 'right'
+				if char == '<':
+					direction = 'left'
+				car = Car(cn, ln, direction, item)
+				item.currentCar = car
+
+			if char == 'v' or char == '^':
+				item = StraightTrack(cn, ln, 0, '|', field[ln-1][cn], 0)
+				if isinstance(field[ln-1][cn], StraightTrack):
+					field[ln-1][cn].after = item
+				elif isinstance(field[ln-1][cn], CrossTrack):
+					field[ln-1][cn].bottom = item
+
+				direction = 'up'
+				if char == 'v':
+					direction = 'down'
+				car = Car(cn, ln, direction, item)
+				item.currentCar = car
+
+			if char == '/':
+				item = StraightTrack(cn, ln, 0, '/', 0, 0)
+
+			if char == '\\':
+				item = StraightTrack(cn, ln, 0, '\\', 0, 0)
+
+			if char == '+':
+				item = CrossTrack(cn, ln, field[ln][cn-1], 0, field[ln-1][cn], 0)
+				if isinstance(field[ln-1][cn], StraightTrack):
+					field[ln-1][cn].after = item
+				elif isinstance(field[ln-1][cn], CrossTrack):
+					field[ln-1][cn].bottom = item
+
+				if isinstance(field[ln][cn-1], StraightTrack):
+					field[ln][cn-1].after = item
+				elif isinstance(field[ln][cn-1], CrossTrack):
+					field[ln][cn-1].right = item
+
+			field[ln][cn] =  item
+			if car != 0:
+				cars.append(car)
+
+
+printField()
